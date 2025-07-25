c123d81974b0021a4016ca5c734ab6ba182a6a07
knyazew.sv@gmail.com <knyazew.sv@gmail.com>
2025-07-25 11:38:51 +0300

    1

--- apps/server/src/products/products.module.ts
+++ apps/server/src/products/products.module.ts
@@ -8,9 +8,10 @@ import { ScheduleModule } from '@nestjs/schedule'
 import { NotificationModule } from '../bot/notification.module'
 import { UserModule } from '../user/user.module'
 import { CronService } from './cron.service'
+import { OrganizationModule } from '../organization/organization.module'
 
 @Module({
-  imports: [ScheduleModule, NotificationModule, UserModule],
+  imports: [ScheduleModule, NotificationModule, UserModule, OrganizationModule],
   controllers: [ProductsController, CronController],
   providers: [PrismaService, ProductsService, BotService, CronService],
   exports: [ProductsService, CronService]
