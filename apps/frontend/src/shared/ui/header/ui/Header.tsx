c71b9bd99f338748eafb51011a1e53c191afb903
knyazew.sv@gmail.com <knyazew.sv@gmail.com>
2025-07-28 15:49:25 +0300

    1

--- apps/frontend/src/shared/ui/header/ui/Header.tsx
+++ apps/frontend/src/shared/ui/header/ui/Header.tsx
@@ -8,22 +8,22 @@ const Header = () => {
   return (
     <header className='relative top-0 z-10 group flex flex-col'>
       <div className='flex items-center  flex-col'>
-        <span className='text-sm text-gray-500 dark:text-neutral-500 text-nowrap'>
-          Текущий склад:
-        </span>
         <div className='relative  flex items-center justify-center w-full'>
           {/* {currentOrganization?.role && (
             <span className='inline-flex absolute bg-blue-600 text-white  -top-3.5 right-0 items-center gap-x-1.5 py-0.5 px-2 rounded-full text-xs font-medium  '>
               {getRole(currentOrganization?.role)}
             </span>
           )} */}
-          <span className='text-sm truncate max-w-[60%]  font-medium text-gray-800 dark:text-neutral-200  relative '>
+          <span className='text-sm truncate max-w-[90%]  font-medium text-gray-800 dark:text-neutral-200  relative '>
+            <span className='text-sm text-gray-500 dark:text-neutral-500 text-nowrap'>
+              Текущий склад:
+            </span>{' '}
             {currentOrganization?.organization?.name}
           </span>
         </div>
         {currentOrganization?.role && (
           <span className='inline-flex mt-1  bg-blue-600 text-white  items-center gap-x-1.5 py-0.5 px-2 rounded-full text-xs font-medium  '>
-            {getRole(currentOrganization?.role)}
+            Роль: {getRole(currentOrganization?.role)}
           </span>
         )}
       </div>
