7353101424cfd0c0bc65c4bfad9a762ea5f04752
knyazew.sv@gmail.com <knyazew.sv@gmail.com>
2025-07-28 15:45:48 +0300

    1

--- apps/frontend/src/pages/menu-page/MenuPage.tsx
+++ apps/frontend/src/pages/menu-page/MenuPage.tsx
@@ -227,238 +227,234 @@ const MenuPage: FC = () => {
       back={false}
       className='!pt-0'
     >
-      <div className='flex flex-col flex-1 pt-4'>
-        <Header />
-        <AlertProductLowStock />
+      <Header />
+      <AlertProductLowStock />
 
-        <Link
-          className='p-4 group mt-4 flex flex-col bg-pink-50 rounded-xl focus:outline-hidden dark:bg-pink-800/40'
-          onClick={() => hapticFeedback.impactOccurred('rigid')}
-          to={'/report'}
-        >
-          <div className='mb-4 flex flex-col justify-center items-center h-full'>
-            <span className='flex justify-center items-center size-12 xl:size-16 mx-auto bg-pink-200/50 text-white rounded-2xl dark:bg-pink-800/30'>
-              <svg
-                className='shrink-0 size-5 xl:w-6 xl:h-6 text-pink-600 dark:text-pink-500'
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-              >
-                <path d='M7 2h10' />
-                <path d='M5 6h14' />
-                <rect
-                  width='18'
-                  height='12'
-                  x='3'
-                  y='10'
-                  rx='2'
-                />
-              </svg>
-            </span>
-          </div>
-          <div className='text-center mt-auto'>
-            <p className='truncate flex justify-center items-center gap-x-1 text-xs xl:text-sm font-medium text-gray-800 group-hover:text-pink-600 group-focus:text-pink-600 dark:text-neutral-200 dark:group-hover:text-neutral-400 dark:group-focus:text-neutral-400'>
-              Расход
-              <svg
-                className='shrink-0 size-3.5'
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-              >
-                <path
-                  className='lg:opacity-0 lg:-translate-x-1 lg:group-hover:opacity-100 lg:group-hover:translate-x-0 lg:group-focus:opacity-100 lg:group-focus:translate-x-0 lg:transition'
-                  d='M5 12h14'
-                />
-                <path
-                  className='lg:-translate-x-1.5 lg:group-hover:translate-x-0 lg:group-focus:translate-x-0 lg:transition'
-                  d='m12 5 7 7-7 7'
-                />
-              </svg>
-            </p>
-          </div>
-        </Link>
-        <Link
-          className='p-4 group mt-4 flex flex-col bg-pink-50 rounded-xl focus:outline-hidden dark:bg-green-800/40'
-          onClick={() => hapticFeedback.impactOccurred('rigid')}
-          to={'/incoming-to-warehouse'}
-        >
-          <div className='mb-4 flex flex-col justify-center items-center h-full'>
-            <span className='flex justify-center items-center size-12 xl:size-16 mx-auto bg-green-200/50 text-white rounded-2xl dark:bg-green-800/30'>
-              <svg
-                className='shrink-0 size-5 xl:w-6 xl:h-6 text-green-600 dark:text-green-500'
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-              >
-                <path d='M7 2h10' />
-                <path d='M5 6h14' />
-                <rect
-                  width='18'
-                  height='12'
-                  x='3'
-                  y='10'
-                  rx='2'
-                />
-              </svg>
-            </span>
-          </div>
-          <div className='text-center mt-auto'>
-            <p className='truncate flex justify-center items-center gap-x-1 text-xs xl:text-sm font-medium text-gray-800 group-hover:text-green-600 group-focus:text-green-600 dark:text-neutral-200 dark:group-hover:text-neutral-400 dark:group-focus:text-neutral-400'>
-              Поступление
-              <svg
-                className='shrink-0 size-3.5'
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-              >
-                <path
-                  className='lg:opacity-0 lg:-translate-x-1 lg:group-hover:opacity-100 lg:group-hover:translate-x-0 lg:group-focus:opacity-100 lg:group-focus:translate-x-0 lg:transition'
-                  d='M5 12h14'
-                />
-                <path
-                  className='lg:-translate-x-1.5 lg:group-hover:translate-x-0 lg:group-focus:translate-x-0 lg:transition'
-                  d='m12 5 7 7-7 7'
-                />
-              </svg>
-            </p>
-          </div>
-        </Link>
-        <div className='w-28 h-px mx-auto bg-gray-300 dark:bg-neutral-700 mt-8' />
-
-        <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 xl:grid-cols-6 gap-2 lg:gap-4 mt-8'>
-          {menuButtons.map(button => (
-            <MenuButton
-              key={button.to}
-              {...button}
-            />
-          ))}
-
-          {isIT && (
-            <Link
-              to={'/'}
-              className='p-4 group flex flex-col bg-white border border-gray-200 rounded-xl focus:outline-hidden dark:bg-neutral-900 dark:border-neutral-700'
-              onClick={() => hapticFeedback.impactOccurred('rigid')}
+      <Link
+        className='p-4 group mt-4 flex flex-col bg-pink-50 rounded-xl focus:outline-hidden dark:bg-pink-800/40'
+        onClick={() => hapticFeedback.impactOccurred('rigid')}
+        to={'/report'}
+      >
+        <div className='mb-4 flex flex-col justify-center items-center h-full'>
+          <span className='flex justify-center items-center size-12 xl:size-16 mx-auto bg-pink-200/50 text-white rounded-2xl dark:bg-pink-800/30'>
+            <svg
+              className='shrink-0 size-5 xl:w-6 xl:h-6 text-pink-600 dark:text-pink-500'
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
             >
-              <div className='mb-4 flex flex-col justify-center items-center h-full'>
-                <span className='flex justify-center items-center size-12 xl:size-16 mx-auto bg-yellow-50 text-white rounded-2xl dark:bg-yellow-800/30'>
-                  <svg
-                    className='shrink-0 size-5 xl:w-6 xl:h-6 text-yellow-600 dark:text-yellow-500'
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='24'
-                    height='24'
-                    viewBox='0 0 24 24'
-                    fill='none'
-                    stroke='currentColor'
-                    strokeWidth='2'
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                  >
-                    <path d='M16 3h5v5' />
-                    <path d='M8 3H3v5' />
-                    <path d='M12 22v-8.3a4 4 0 0 0-1.172-2.872L3 3' />
-                    <path d='m15 9 6-6' />
-                  </svg>
-                </span>
-              </div>
-              <div className='text-center mt-auto'>
-                <p className='truncate text-xs xl:text-sm font-medium text-gray-800 group-hover:text-pink-600 group-focus:text-pink-600 dark:text-neutral-200 dark:group-hover:text-neutral-400 dark:group-focus:text-neutral-400'>
-                  /auth
-                </p>
-              </div>
-            </Link>
-          )}
-
-          {/* End Card */}
+              <path d='M7 2h10' />
+              <path d='M5 6h14' />
+              <rect
+                width='18'
+                height='12'
+                x='3'
+                y='10'
+                rx='2'
+              />
+            </svg>
+          </span>
         </div>
-        <div className='w-28 h-px mx-auto bg-gray-300 dark:bg-neutral-700 mt-8' />
-
-        <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 xl:grid-cols-6 gap-2 lg:gap-4 mt-8'>
-          <MenuButton
-            to='/info-page'
-            title='Информация'
-            color='zinc'
-            icon={
-              <Info className='shrink-0 size-5 xl:w-6 xl:h-6 text-zinc-600 dark:text-zinc-500' />
-            }
-          />
-
-          <button
-            onClick={() => {
-              openTelegramLink('https://t.me/Knyaz_sv')
-              hapticFeedback.impactOccurred('rigid')
-            }}
-            className='p-4 group relative overflow-hidden flex flex-col bg-white border border-gray-200 rounded-xl focus:outline-hidden dark:bg-neutral-900 dark:border-neutral-700'
-          >
-            <span
-              className={clsx(
-                'flex justify-center relative items-center size-12 xl:size-16 mx-auto text-orange-600 dark:text-orange-500 rounded-2xl bg-orange-50 dark:bg-orange-900'
-              )}
+        <div className='text-center mt-auto'>
+          <p className='truncate flex justify-center items-center gap-x-1 text-xs xl:text-sm font-medium text-gray-800 group-hover:text-pink-600 group-focus:text-pink-600 dark:text-neutral-200 dark:group-hover:text-neutral-400 dark:group-focus:text-neutral-400'>
+            Расход
+            <svg
+              className='shrink-0 size-3.5'
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
             >
-              <MessageCircle className='shrink-0 size-5 xl:w-6 xl:h-6 text-orange-600 dark:text-orange-500' />
-            </span>
+              <path
+                className='lg:opacity-0 lg:-translate-x-1 lg:group-hover:opacity-100 lg:group-hover:translate-x-0 lg:group-focus:opacity-100 lg:group-focus:translate-x-0 lg:transition'
+                d='M5 12h14'
+              />
+              <path
+                className='lg:-translate-x-1.5 lg:group-hover:translate-x-0 lg:group-focus:translate-x-0 lg:transition'
+                d='m12 5 7 7-7 7'
+              />
+            </svg>
+          </p>
+        </div>
+      </Link>
+      <Link
+        className='p-4 group mt-4 flex flex-col bg-pink-50 rounded-xl focus:outline-hidden dark:bg-green-800/40'
+        onClick={() => hapticFeedback.impactOccurred('rigid')}
+        to={'/incoming-to-warehouse'}
+      >
+        <div className='mb-4 flex flex-col justify-center items-center h-full'>
+          <span className='flex justify-center items-center size-12 xl:size-16 mx-auto bg-green-200/50 text-white rounded-2xl dark:bg-green-800/30'>
+            <svg
+              className='shrink-0 size-5 xl:w-6 xl:h-6 text-green-600 dark:text-green-500'
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            >
+              <path d='M7 2h10' />
+              <path d='M5 6h14' />
+              <rect
+                width='18'
+                height='12'
+                x='3'
+                y='10'
+                rx='2'
+              />
+            </svg>
+          </span>
+        </div>
+        <div className='text-center mt-auto'>
+          <p className='truncate flex justify-center items-center gap-x-1 text-xs xl:text-sm font-medium text-gray-800 group-hover:text-green-600 group-focus:text-green-600 dark:text-neutral-200 dark:group-hover:text-neutral-400 dark:group-focus:text-neutral-400'>
+            Поступление
+            <svg
+              className='shrink-0 size-3.5'
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            >
+              <path
+                className='lg:opacity-0 lg:-translate-x-1 lg:group-hover:opacity-100 lg:group-hover:translate-x-0 lg:group-focus:opacity-100 lg:group-focus:translate-x-0 lg:transition'
+                d='M5 12h14'
+              />
+              <path
+                className='lg:-translate-x-1.5 lg:group-hover:translate-x-0 lg:group-focus:translate-x-0 lg:transition'
+                d='m12 5 7 7-7 7'
+              />
+            </svg>
+          </p>
+        </div>
+      </Link>
+      <div className='w-28 h-px mx-auto bg-gray-300 dark:bg-neutral-700 mt-8' />
 
-            <div className='text-center  mt-2'>
+      <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 xl:grid-cols-6 gap-2 lg:gap-4 mt-8'>
+        {menuButtons.map(button => (
+          <MenuButton
+            key={button.to}
+            {...button}
+          />
+        ))}
+
+        {isIT && (
+          <Link
+            to={'/'}
+            className='p-4 group flex flex-col bg-white border border-gray-200 rounded-xl focus:outline-hidden dark:bg-neutral-900 dark:border-neutral-700'
+            onClick={() => hapticFeedback.impactOccurred('rigid')}
+          >
+            <div className='mb-4 flex flex-col justify-center items-center h-full'>
+              <span className='flex justify-center items-center size-12 xl:size-16 mx-auto bg-yellow-50 text-white rounded-2xl dark:bg-yellow-800/30'>
+                <svg
+                  className='shrink-0 size-5 xl:w-6 xl:h-6 text-yellow-600 dark:text-yellow-500'
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='24'
+                  height='24'
+                  viewBox='0 0 24 24'
+                  fill='none'
+                  stroke='currentColor'
+                  strokeWidth='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                >
+                  <path d='M16 3h5v5' />
+                  <path d='M8 3H3v5' />
+                  <path d='M12 22v-8.3a4 4 0 0 0-1.172-2.872L3 3' />
+                  <path d='m15 9 6-6' />
+                </svg>
+              </span>
+            </div>
+            <div className='text-center mt-auto'>
               <p className='truncate text-xs xl:text-sm font-medium text-gray-800 group-hover:text-pink-600 group-focus:text-pink-600 dark:text-neutral-200 dark:group-hover:text-neutral-400 dark:group-focus:text-neutral-400'>
-                Поддержка
+                /auth
               </p>
             </div>
-          </button>
-          <MenuButton
-            to='/settings'
-            title='Настройки'
-            color='gray'
-            isDevelop={true}
-            icon={
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='shrink-0 size-5 xl:w-6 xl:h-6 text-gray-600 dark:text-gray-500'
-                width={24}
-                height={24}
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth={2}
-                strokeLinecap='round'
-                strokeLinejoin='round'
-              >
-                <path d='M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z' />
-                <circle
-                  cx={12}
-                  cy={12}
-                  r={3}
-                />
-              </svg>
-            }
-          />
+          </Link>
+        )}
 
-          {/* <BottomSheetSupport /> */}
-        </div>
-        {/* End Grid */}
+        {/* End Card */}
       </div>
+      <div className='w-28 h-px mx-auto bg-gray-300 dark:bg-neutral-700 mt-8' />
+
+      <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 xl:grid-cols-6 gap-2 lg:gap-4 mt-8'>
+        <MenuButton
+          to='/info-page'
+          title='Информация'
+          color='zinc'
+          icon={<Info className='shrink-0 size-5 xl:w-6 xl:h-6 text-zinc-600 dark:text-zinc-500' />}
+        />
+
+        <button
+          onClick={() => {
+            openTelegramLink('https://t.me/Knyaz_sv')
+            hapticFeedback.impactOccurred('rigid')
+          }}
+          className='p-4 group relative overflow-hidden flex flex-col bg-white border border-gray-200 rounded-xl focus:outline-hidden dark:bg-neutral-900 dark:border-neutral-700'
+        >
+          <span
+            className={clsx(
+              'flex justify-center relative items-center size-12 xl:size-16 mx-auto text-orange-600 dark:text-orange-500 rounded-2xl bg-orange-50 dark:bg-orange-900'
+            )}
+          >
+            <MessageCircle className='shrink-0 size-5 xl:w-6 xl:h-6 text-orange-600 dark:text-orange-500' />
+          </span>
+
+          <div className='text-center  mt-2'>
+            <p className='truncate text-xs xl:text-sm font-medium text-gray-800 group-hover:text-pink-600 group-focus:text-pink-600 dark:text-neutral-200 dark:group-hover:text-neutral-400 dark:group-focus:text-neutral-400'>
+              Поддержка
+            </p>
+          </div>
+        </button>
+        <MenuButton
+          to='/settings'
+          title='Настройки'
+          color='gray'
+          isDevelop={true}
+          icon={
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='shrink-0 size-5 xl:w-6 xl:h-6 text-gray-600 dark:text-gray-500'
+              width={24}
+              height={24}
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth={2}
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            >
+              <path d='M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z' />
+              <circle
+                cx={12}
+                cy={12}
+                r={3}
+              />
+            </svg>
+          }
+        />
+
+        {/* <BottomSheetSupport /> */}
+      </div>
+      {/* End Grid */}
     </Page>
   )
 }
