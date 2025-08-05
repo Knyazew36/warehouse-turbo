1600e57063fcf31d14325ce222d34ce8ebdb4be4
Sergey Knyazew <knyazew.sv@gmail.com>
2025-08-05 19:12:41 +0300

    1

--- apps/frontend/src/entitites/category/ui/category-select-modal/CategorySelectModal.tsx
+++ apps/frontend/src/entitites/category/ui/category-select-modal/CategorySelectModal.tsx
@@ -11,13 +11,67 @@ import {
 } from '@/components/ui/dialog'
 
 import Select from 'react-select'
+import Divide from '@/shared/ui/divide/ui/Divide'
+import { Controller, useForm } from 'react-hook-form'
+import { useCreateCategory } from '../../api/category.api'
+import { hapticFeedback } from '@telegram-apps/sdk-react'
+import { useBottomSheetStore } from '@/shared/bottom-sheet/model/store.bottom-sheet'
+import InputDefault from '@/shared/ui/input-default/ui/InputDefault'
+import { useQueries, useQueryClient } from '@tanstack/react-query'
 
 interface IProps {
   data: ISelectOption[]
 }
+type FormValues = {
+  name: string
+  description: string
+  color: string
+  icon: string
+}
 
 const CategorySelectModal = ({ data }: IProps) => {
   const [isOpen, setIsOpen] = useState(false)
+  const { open } = useBottomSheetStore()
+  const queryClient = useQueryClient()
+
+  const {
+    register,
+    control,
+    handleSubmit,
+    reset,
+    watch,
+    formState: { errors, isSubmitting, isValid }
+  } = useForm<FormValues>({
+    mode: 'onSubmit',
+    defaultValues: { name: '' }
+  })
+  const { mutateAsync: createCategory } = useCreateCategory()
+  const [buttonLoading, setButtonLoading] = useState(false)
+
+  const onSubmit = async (data: FormValues) => {
+    try {
+      setButtonLoading(true)
+      await createCategory({
+        name: data.name.trim(),
+        description: '',
+        color: '',
+        icon: ''
+      })
+
+      open({
+        isOpen: true,
+        description: 'Категория успешно создана. Теперь вы можете выбрать ее из списка.'
+      })
+
+      queryClient.invalidateQueries({ queryKey: ['category-select-options'] })
+      hapticFeedback.notificationOccurred('success')
+    } catch (e: any) {
+      hapticFeedback.notificationOccurred('error')
+    } finally {
+      setButtonLoading(false)
+      // Сбрасываем форму после успешного создания
+    }
+  }
 
   // Custom styles for react-select to match the app's design system
   const customStyles = {
@@ -142,16 +196,53 @@ const CategorySelectModal = ({ data }: IProps) => {
         <DialogHeader>
           <DialogTitle>Категории</DialogTitle>
         </DialogHeader>
-        <DialogDescription>Выберите категорию для товара или создайте новую</DialogDescription>
 
-        <Select
-          options={data}
-          isMulti
-          className='w-full'
-          styles={customStyles}
-          placeholder='Выберите категории...'
-          noOptionsMessage={() => 'Нет доступных категорий'}
-        />
+        <div className='flex flex-col gap-1'>
+          <label className='inline-block text-sm text-gray-500 sm:mt-2.5 dark:text-neutral-500'>
+            Выберите категории
+          </label>
+
+          <Select
+            options={data}
+            // onChange={value => {
+            //   console.log(value)
+            // }}
+            // value={watch('name')}
+            // isMulti
+            className='w-full'
+            styles={customStyles}
+            placeholder='Выберите категории...'
+            noOptionsMessage={() => 'Нет доступных категорий'}
+          />
+        </div>
+
+        <div className='mx-auto my-2 h-px w-28 bg-gray-300 dark:bg-neutral-700' />
+
+        <form
+          className='flex flex-col gap-2'
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <Controller
+            control={control}
+            name='name'
+            rules={{
+              required: 'Название обязательно',
+              validate: value => {
+                if (!value || value.trim().length === 0) {
+                  return 'Название обязательно'
+                }
+              }
+            }}
+            render={({ field }) => (
+              <InputDefault
+                label='Или создайте новую'
+                {...field}
+                placeholder='Категория'
+                error={errors.name?.message}
+              />
+            )}
+          />
+        </form>
 
         <div className='flex items-center justify-end gap-x-2 border-t border-gray-200 py-3 dark:border-neutral-700'>
           <DialogClose>
